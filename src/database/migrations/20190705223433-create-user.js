a56de983ea040c9c360c0bf0d8095cd911f6f1ef
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2019-07-06 00:42:42 +0100

    Created the users table

--- src/database/migrations/20190705223433-create-user.js
+++ src/database/migrations/20190705223433-create-user.js
@@ -6,23 +6,89 @@ module.exports = {
 				allowNull: false,
 				autoIncrement: true,
 				primaryKey: true,
-				type: Sequelize.INTEGER
+				type: Sequelize.INTEGER.UNSIGNED
 			},
 			firstName: {
-				type: Sequelize.STRING
+				type: Sequelize.STRING(30),
+				allowNull: false
 			},
 			lastName: {
-				type: Sequelize.STRING
+				type: Sequelize.STRING(30),
+				allowNull: false
+			},
+			username: {
+				type: Sequelize.STRING(30),
+				allowNull: false,
+				unique: true
 			},
 			email: {
-				type: Sequelize.STRING
+				type: Sequelize.STRING(30),
+				allowNull: false,
+				unique: true,
+			},
+			wallet: {
+				type: Sequelize.DECIMAL(10, 2).UNSIGNED,
+				allowNull: false,
+				defaultValue: 0
 			},
 			password: {
-				type: Sequelize.STRING
+				type: Sequelize.STRING(100),
+				allowNull: false
 			},
-			
 			phoneNumber: {
-				type: Sequelize.STRING
+				type: Sequelize.STRING(11)
+			},
+			whatsappNumber: {
+				type: Sequelize.STRING(11)
+			},
+			bankAccountNumber: {
+				type: Sequelize.STRING(16)
+			},
+			bankAccountName: {
+				type: Sequelize.STRING(30)
+			},
+			bankId: {
+				type: Sequelize.INTEGER(2)
+			},
+			bankAccountType: {
+				type: Sequelize.STRING(10)
+			},
+			pv: {
+				type: Sequelize.MEDIUMINT(7).UNSIGNED,
+				allowNull: false,
+				defaultValue: 0
+			},
+			cumulativePv: {
+				type: Sequelize.MEDIUMINT(7).UNSIGNED,
+				allowNull: false,
+				defaultValue: 0
+			},
+			pvDate: {
+				type: Sequelize.DATE
+			},
+			transactions: {
+				type: Sequelize.TEXT("long")
+			},
+			upFront: {
+				type: Sequelize.TINYINT(2).UNSIGNED
+			},
+			image: {
+				type: Sequelize.STRING(100)
+			},
+			packageId: {
+				type: Sequelize.TINYINT(2).UNSIGNED
+			},
+			referralId: {
+				type: Sequelize.STRING(30)
+			},
+			ragpReferralId: {
+				type: Sequelize.STRING(30)
+			},
+			paid: {
+				type: Sequelize.DATE
+			},
+			aboutMe: {
+				type: Sequelize.STRING(160)
 			},
 			createdAt: {
 				allowNull: false,
